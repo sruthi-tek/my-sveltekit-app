7f5c45452b61362fd3378eb4e00168f3e6171c77
Sruthi S <sruthi@MacBookPro.attlocal.net>
2025-07-29 04:19:16 -0500

    testing precommit hook

--- src/routes/page.svelte.test.ts
+++ src/routes/page.svelte.test.ts
@@ -7,7 +7,9 @@ describe('/+page.svelte', () => {
 	it('should render h1', async () => {
 		render(Page);
 
-		const heading = page.getByRole('heading', { level: 1 });
+		const heading = page.getByRole('heading', {
+			level: 1
+		});
 		await expect.element(heading).toBeInTheDocument();
 	});
 });
